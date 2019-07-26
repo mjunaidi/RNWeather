0814aa0175551300aa46cc708d5d4c9f6517dd52
mjunaidi <mjunaidijusoh-82@yahoo.com>
2019-07-26 10:51:41 +0800

    Dynamic font size for temperature unit

--- src/component/Temp.js
+++ src/component/Temp.js
@@ -2,19 +2,38 @@ import React from 'react'
 import {TouchableOpacity,Text} from 'react-native'
 import {k2c,k2f} from '../Util'
 
+const fontSizeFrom = (style)=>{
+  if (Array.isArray(style)&&style.length>0) {
+    for (let i=0;i<style.length;i++) {
+      const {fontSize} = style[i]
+      if (typeof(fontSize)==='number'&&fontSize>0) {
+        return fontSize
+      }
+    }
+  }
+  return 14
+}
+
 export default function Temp(props) {
   const {style,onPress,value,celcius=false} = props
+  const fontSize = fontSizeFrom(style)*2/3
 
   if (celcius) {
     return (
       <TouchableOpacity onPress={onPress}>
-        <Text style={style}>{k2c(value)} °C</Text>
+        <Text style={style}>
+          <Text>{k2c(value)}</Text>
+          <Text style={{fontSize}}> °C</Text>
+        </Text>
       </TouchableOpacity>
     )
   }
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text style={style}>{k2f(value)} °F</Text>
+      <Text style={style}>
+        <Text>{k2f(value)}</Text>
+        <Text style={{fontSize}}> °F</Text>
+      </Text>
     </TouchableOpacity>
   )
 }
